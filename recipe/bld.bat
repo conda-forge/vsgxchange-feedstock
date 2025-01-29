e5dbfda6e798b6ec5a74c5391e831789625a21d6
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-01-29 08:41:46 -0800

    vsgXchange_OSG=ON

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,8 @@ cmake %SRC_DIR% ^
   -DBUILD_SHARED_LIBS=ON ^
   -DvsgXchange_assimp=ON ^
   -DvsgXchange_freetype=ON ^
-  -DvsgXchange_openexr=ON
+  -DvsgXchange_openexr=ON ^
+  -DvsgXchange_OSG=ON
 if errorlevel 1 exit 1
 
 cmake --build build --parallel --config Release
