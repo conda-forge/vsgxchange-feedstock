411467531b18dd507aa4863310e65416ec744337
Jeongseok Lee <jeongseok@meta.com>
2025-11-28 10:21:01 -0800

    Switch to Ninja generator on Windows to fix build failure

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,11 @@
 @echo on
 
 cmake %SRC_DIR% ^
+  -G "Ninja" ^
   %CMAKE_ARGS% ^
   -B build ^
   -DBUILD_SHARED_LIBS=ON ^
+  -DCMAKE_BUILD_TYPE=Release ^
   -DvsgXchange_assimp=ON ^
   -DvsgXchange_freetype=ON ^
   -DvsgXchange_openexr=ON ^
@@ -11,8 +13,8 @@ cmake %SRC_DIR% ^
   -DvsgXchange_OSG=ON
 if errorlevel 1 exit 1
 
-cmake --build build --parallel --config Release
+cmake --build build --parallel
 if errorlevel 1 exit 1
 
-cmake --install build --config Release
+cmake --install build
 if errorlevel 1 exit 1
